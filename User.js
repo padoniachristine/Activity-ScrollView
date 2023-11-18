6d5bda51433f0da16d76e1eb331aa21d42c11007
padoniachristine <143053303+padoniachristine@users.noreply.github.com>
2023-11-18 03:11:15 -1200

    Add files via upload
    Padonia-ScrollView

---
+++ User.js
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "CHRISTINE",
+        middlename: "N",
+        lastname: "PADONIA",
+      },
+      address: "De La Paz, Cortes",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "ANALIE",
+        middlename: "V",
+        lastname: "BARBANIDA",
+      },
+      address: "Cabawan, Maribojoc",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "LAIZA",
+        middlename: "",
+        lastname: "HANGAD",
+      },
+      address: "Del Carmen Weste, Balilihan",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "JUNAH",
+        middlename: "J",
+        lastname: "MASLOG",
+      },
+      address: "Manga District, Tagbilaran City",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "QUENNIE",
+        middlename: "",
+        lastname: "EULLONE",
+      },
+      address: "Agahay, Maribojoc",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "JEA MARIE",
+        middlename: "",
+        lastname: "OCHEA",
+      },
+      address: "Mansasa, Tagbilaran City",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "ERECA",
+        middlename: "",
+        lastname: "GREPO",
+      },
+      address: "Mayacabac, Dauis",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "CARA",
+        middlename: "",
+        lastname: "ARANZADO",
+      },
+      address: "Tinago, Dauis",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "JAQUELINE",
+        middlename: "",
+        lastname: "VILLOREJO",
+      },
+      address: "Cabawan, Maribojoc",
+    },
+  ];
